877bf0b93c3619c8a2d93838ad0c2e6c8dfbcd3c
Ellen Park <ellenpark781@gmail.com>
2020-10-01 14:33:20 -0400

    created component files

---
+++ react-practice/src/Board.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Board.css'; 
+import List from './List';
+
+export default class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    if (localStorage.getItem('lists')) {
+      const rawLS = localStorage.getItem('lists'); 
+      const parsedLS = JSON.parse(rawLS);
+      this.state = {list: parsedLS}
+    } else {
+      this.state = {
+        {
+          title: ''
+        }
+      }
+    }
+  }
+}
